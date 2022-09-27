458a241555fdde9667ac71f3dbcdac3fc2056c9f
Szymon Zimecki (Uczeń) <szimecki195@technikumkreatywne.pl>
2022-09-27 10:11:25 +0200

    Tablice

---
+++ Lekcja_7/tablice.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Strona szefa</title>
+</head>
+<body>
+
+	<?php
+	
+		$tablica = array("Harry", "Hobbit", "Zemsta", "Zasady ");
+
+		
+		for($j = 4; $j > 0; --$j){
+			
+			echo $tablica[$j] . "<br>";
+			
+		}
+		
+		
+		
+	?>
+
+
+</body>
