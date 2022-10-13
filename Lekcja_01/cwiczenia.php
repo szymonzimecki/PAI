75d369e3e79471d3860f415527a3c62ddb92403b
szymonzimecki <blckus4@gmail.com>
2022-10-13 08:51:23 +0200

    Ćwiczenia

---
+++ Lekcja_01/cwiczenia.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Strona szefa</title>
+</head>
+<body>
+
+	<?php
+
+		$mycounter = 1;
+
+		echo "Hello World" . "<br>" . $mycounter . "<br>" . "Hello Friend"
+
+	?>
+
+
+</body>
+</html>
